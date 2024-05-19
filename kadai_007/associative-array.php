959c56bc63e5c915b1b3f2a01890199262b0a444
HIROKI-NAGAMINE <159559981+HIROKI-NAGAMINE@users.noreply.github.com>
2024-05-19 09:00:28 +0900

    課題のアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang='ja'>
+
+  <head>
+    <meta charset="UTF-8">
+    <title>PHP課題7</title>
+  </head>
+  <body>
+    <p>
+        <?php
+          //連想配列に値を代入する
+          $product_data = ['name'=>'onion','price'=>200,'weight'=>160];
+
+          //連想配列の値を出力する
+          print_r($product_data);
+
+          
+        ?>
+    </p>
+
+
+  </body>
+
+
+</html>
